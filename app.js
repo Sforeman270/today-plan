771919e194c20a3580d8fad2b2c47ef3f8e28aae
Sforeman270 <65334884+Sforeman270@users.noreply.github.com>
2020-07-25 20:45:43 -0500

    time grid

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var $date = $(".date");
     var $inputForm = $(".inputs");
     
-    var hours = ["9 am","10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm"];
+    var hours = ["9 am","10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
     var currentHour = moment().format("H");
 
     $date.text(moment().format("llll"));
